503887fcddae9f352c945960417ac3753a464fea
Andy Pan <andy0130tw@yahoo.com.tw>
2018-12-11 00:45:09 +0800

    Update images / manifest

--- ui/app/components/mascot.js
+++ ui/app/components/mascot.js
@@ -9,51 +9,19 @@ module.exports = Mascot
 inherits(Mascot, Component)
 function Mascot ({width = '200', height = '200'}) {
   Component.call(this)
-  this.logo = metamaskLogo({
-    followMouse: true,
-    pxNotRatio: true,
-    width,
-    height,
-  })
-
-  this.refollowMouse = debounce(this.logo.setFollowMouse.bind(this.logo, true), 1000)
-  this.unfollowMouse = this.logo.setFollowMouse.bind(this.logo, false)
+  this.width = width
+  this.height = height
 }
 
 Mascot.prototype.render = function () {
-  // this is a bit hacky
-  // the event emitter is on `this.props`
-  // and we dont get that until render
-  this.handleAnimationEvents()
 
   return h('#metamask-mascot-container', {
-    style: { zIndex: 0 },
+    style: {
+      zIndex: 0,
+      backgroundImage: 'url(images/metamonk.svg)',
+      backgroundSize: 'contain',
+      width: this.width,
+      height: this.height,
+    },
   })
-}
-
-Mascot.prototype.componentDidMount = function () {
-  var targetDivId = 'metamask-mascot-container'
-  var container = document.getElementById(targetDivId)
-  container.appendChild(this.logo.container)
-}
-
-Mascot.prototype.componentWillUnmount = function () {
-  this.animations = this.props.animationEventEmitter
-  this.animations.removeAllListeners()
-  this.logo.container.remove()
-  this.logo.stopAnimation()
-}
-
-Mascot.prototype.handleAnimationEvents = function () {
-  // only setup listeners once
-  if (this.animations) return
-  this.animations = this.props.animationEventEmitter
-  this.animations.on('point', this.lookAt.bind(this))
-  this.animations.on('setFollowMouse', this.logo.setFollowMouse.bind(this.logo))
-}
-
-Mascot.prototype.lookAt = function (target) {
-  this.unfollowMouse()
-  this.logo.lookAt(target)
-  this.refollowMouse()
 }
